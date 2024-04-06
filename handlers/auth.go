3b17f452329be6f52f4e83c92de7d59420960230
surfer <surfer@surfers-MacBook-Pro.local>
2024-04-06 22:05:01 +0300

    done with sessions

--- handlers/auth.go
+++ handlers/auth.go
@@ -4,7 +4,6 @@ import (
 	"context"
 	"crypto/sha256"
 	"net/http"
-	"os"
 	"recipes-api/models"
 	"time"
 
@@ -71,6 +70,25 @@ func (handler *AuthHandler) SignInHandler(c *gin.Context) {
 	c.JSON(http.StatusOK, gin.H{"message": "User signed in"})
 }
 
+func (handler *AuthHandler) RefreshHandler(c *gin.Context) {
+
+	session := sessions.Default(c)
+	sessionToken := session.Get("token")
+	sessionUser := session.Get("username")
+
+	if sessionToken == nil {
+		c.JSON(http.StatusUnauthorized, gin.H{"error": "Invalid session cookie"})
+		return
+	}
+
+	sessionToken = xid.New().String()
+	session.Set("username", sessionUser.(string))
+	session.Set("token", sessionToken)
+	session.Save()
+
+	c.JSON(http.StatusOK, gin.H{"message": "New session issued"})
+}
+
 func (handler *AuthHandler) AuthMiddleware() gin.HandlerFunc {
 	return func(c *gin.Context) {
 		session := sessions.Default(c)
@@ -83,61 +101,5 @@ func (handler *AuthHandler) AuthMiddleware() gin.HandlerFunc {
 			c.Abort()
 		}
 		c.Next()
-
-		/*
-			tokenValue := c.GetHeader("Authorization")
-
-			claims := &Claims{}
-
-			tkn, err := jwt.ParseWithClaims(tokenValue, claims,
-				func(token *jwt.Token) (interface{}, error) {
-					return []byte(os.Getenv("JWT_SECRET")), nil
-				})
-
-			if err != nil {
-				c.AbortWithStatus(http.StatusUnauthorized)
-			}
-			if tkn == nil || !tkn.Valid {
-				c.AbortWithStatus(http.StatusUnauthorized)
-			}
-		*/
 	}
-}
-
-func (handler *AuthHandler) RefreshHandler(c *gin.Context) {
-	tokenValue := c.GetHeader("Authorization")
-	claims := &Claims{}
-	tkn, err := jwt.ParseWithClaims(tokenValue, claims, func(token *jwt.Token) (interface{}, error) {
-		return []byte(os.Getenv("JWT_SECRET")), nil
-	})
-	if err != nil {
-		c.JSON(http.StatusUnauthorized, gin.H{"error": err.Error()})
-		return
-	}
-	if tkn == nil || !tkn.Valid {
-		c.JSON(http.StatusUnauthorized, gin.H{"error": "Invalid token"})
-		return
-	}
-
-	if time.Until(time.Unix(claims.ExpiresAt.Unix(), 0)) > 30*time.Second {
-		c.JSON(http.StatusBadRequest, gin.H{"error": "Token is not expired yet"})
-		return
-	}
-
-	expirationTime := time.Now().Add(5 * time.Minute)
-	claims.ExpiresAt = jwt.NewNumericDate(expirationTime)
-	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
-	tokenString, err := token.SignedString(os.Getenv("JWT_SECRET"))
-
-	if err != nil {
-		c.JSON(http.StatusInternalServerError,
-			gin.H{"error": err.Error()})
-		return
-	}
-
-	jwtOutput := JWTOutput{
-		Token:   tokenString,
-		Expires: expirationTime,
-	}
-	c.JSON(http.StatusOK, jwtOutput)
 }
